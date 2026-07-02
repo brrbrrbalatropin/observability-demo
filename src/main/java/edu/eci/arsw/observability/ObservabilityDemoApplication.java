3d6c154121ed2450096fdab491a46663674f029f
Daniel Felipe Rayo Rodriguez <daniel.rayo@mail.escuelaing.edu.co>
2026-07-02 08:53:21 -0500

    añadido aplication yml, ordercontroller y globalexceptionhandler

--- src/main/java/edu/eci/arsw/observability/ObservabilityDemoApplication.java
+++ src/main/java/edu/eci/arsw/observability/ObservabilityDemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ObservabilityDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ObservabilityDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ObservabilityDemoApplication.class, args);
+    }
 
 }
